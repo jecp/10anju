5b3083178a5e5a32326373ac3248e28ac6f62fe1
jecp <phpusr@126.com>
2015-08-11 01:43:32 +0800

    forum's name

--- public/modules/forums/config/forums.client.config.js
+++ public/modules/forums/config/forums.client.config.js
@@ -4,7 +4,7 @@
 angular.module('forums').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', '社区论坛', 'forums', 'dropdown', '/forums(/create)?');
+		Menus.addMenuItem('topbar', '业主论坛', 'forums', 'dropdown', '/forums(/create)?');
 		Menus.addSubMenuItem('topbar', 'forums', '论坛首页', 'forums');
 		Menus.addSubMenuItem('topbar', 'forums', '新的主题', 'subjects/create');
 	}
