9e5171a7d2f24a187e33af2db83415ea98b75d7b
jecp <phpusr@126.com>
2015-08-13 19:51:15 +0800

    201508131951

--- app/controllers/alipay.server.controller.js
+++ app/controllers/alipay.server.controller.js
@@ -34,21 +34,21 @@ var AlipayConfig = {
     service: 'create_direct_pay_by_user',
 
     // 合作身份者ID，以2088开头由16位纯数字组成的字符串
-    partner:'2088911712697575',
+    partner:'',
 
 // 交易安全检验码，由数字和字母组成的32位字符串
-    key:'ogszyddyzb8140o1pr967e06fvndlp6t',
+    key:'',
 
 // 签约支付宝账号或卖家收款支付宝帐户
-    seller_email:'havemay@126.com',
+    seller_email:'',
 
 // 支付宝服务器通知的页面 要用 http://格式的完整路径，不允许加?id:123这类自定义参数
 // 必须保证其地址能够在互联网中访问的到
-    notify_url:'http://www.havemay.cn/#!/paynotify',
+    notify_url:'http://www.havemay.cn/paynotify',
 
 // 当前页面跳转后的页面 要用 http://格式的完整路径，不允许加?id:123这类自定义参数
 // 域名不能写成http://localhost/create_direct_pay_by_user_jsp_utf8/return_url.jsp ，否则会导致return_url执行无效
-    return_url:'http://www.havemay.cn/#!/payreturn',
+    return_url:'http://www.havemay.cn/payreturn',
 
 //      支付宝通知验证地址
 
